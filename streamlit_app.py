071752b91bfc16d08179cca7b2b4d389782c936b
Zachary Fregin <zach.fregin@gmail.com>
2022-10-07 15:53:52 -0700

    Add option

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("Parents New Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Faves")
+streamlit.text("🫕 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header("🍌🍓 Build Your Own Fruit Smoothie 🥝🥭")
