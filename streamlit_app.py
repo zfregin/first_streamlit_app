c45114876791060afe93f1c9609d9e07ccdbd5ff
Zachary Fregin <zach.fregin@gmail.com>
2022-10-07 15:45:05 -0700

    Add Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title("Parents New Diner")
+
+streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
