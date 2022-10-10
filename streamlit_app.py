e81d5b84a2c132bfdaf58b2806827d3ba06bb25e
Zachary Fregin <zach.fregin@gmail.com>
2022-10-10 16:48:51 -0700

    Add FruityVice input and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# add normalized fruityvice json response into dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display dataframe
+streamlit.dataframe(fruityvice_normalized)
